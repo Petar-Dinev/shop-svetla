1afba7a8387909bfec980f5e3def5338c9c7134b
Petar-Dinev <p.dinev55@gmail.com>
2024-01-25 14:05:41 +0200

    feat: react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Contacts from "./components/Contacts";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -17,19 +18,18 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <Link to='/shop-svetla/'>Home</Link>
+      {" | "}
+      <Link to='/shop-svetla/contacts'>Contacts</Link>
+      <Routes>
+        <Route path="/shop-svetla/" element={<Home />} />
+        <Route path="/shop-svetla/contacts" element={<Contacts />} />
+      </Routes>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
